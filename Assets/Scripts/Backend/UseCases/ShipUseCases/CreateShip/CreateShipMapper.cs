1ab878b87f384c5dd4b6dde0877b09f0e7f2bb73
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-10-14 22:13:45 +1100

    Update CreateShipMapper.cs

--- Assets/Scripts/Backend/UseCases/ShipUseCases/CreateShip/CreateShipMapper.cs
+++ Assets/Scripts/Backend/UseCases/ShipUseCases/CreateShip/CreateShipMapper.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Dynamic;
+using System.Collections.Generic;
+using System.Linq;
 using ProjectExodus.Backend.JsonDataContext;
 using ProjectExodus.Domain.Entities;
 using ProjectExodus.Domain.Models;
@@ -14,14 +15,16 @@ namespace ProjectExodus.Backend.UseCases.ShipUseCases.CreateShip
         #region - - - - - - Fields - - - - - -
 
         private readonly IDataContext m_DataContext;
+        private readonly IObjectMapper m_Mapper;
 
         #endregion Fields
 
         #region - - - - - - Constructors - - - - - -
 
-        public CreateShipMapper(IDataContext dataContext, IObjectMapperRegister objectMapperRegister)
+        public CreateShipMapper(IDataContext dataContext, IObjectMapper mapper, IObjectMapperRegister mapperRegister)
         {
             this.m_DataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
+            this.m_Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
         #endregion Constructors
@@ -36,7 +39,13 @@ namespace ProjectExodus.Backend.UseCases.ShipUseCases.CreateShip
 
         private void MapShipEntityToShipModel(Ship source, ShipModel destination)
         {
+            List<WeaponModel> _Weapons = this.m_DataContext
+                .GetEntities<Weapon>()
+                .Select(w => this.m_Mapper.Map(w, new WeaponModel()))
+                .ToList();
             
+            destination.ID = source.ID;
+            destination.Weapons = _Weapons;
         }
 
         #endregion Methods
