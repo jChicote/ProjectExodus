3612c7e6ab1b9d215624bdaebe20c9f4b18e3684
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-10-28 01:36:41 +1100

    Add new AssemblyDefinitions and Nugets for Moq

--- Assets/Tests/Backend/UseCases/ShipUseCases/CreateShipUseCase/CreateShipInteractorTests.cs
+++ Assets/Tests/Backend/UseCases/ShipUseCases/CreateShipUseCase/CreateShipInteractorTests.cs
@@ -1,4 +1,7 @@
+using Moq;
 using NUnit.Framework;
+using ProjectExodus.Backend.UseCases;
+using ProjectExodus.Backend.UseCases.ShipUseCases.CreateShip;
 
 namespace Tests.Backend.UseCases.ShipUseCases.CreateShipUseCase
 {
@@ -6,6 +9,12 @@ namespace Tests.Backend.UseCases.ShipUseCases.CreateShipUseCase
     public class CreateShipInteractorTests
     {
 
+        #region - - - - - - Fields - - - - - -
+
+        private Mock<IUseCaseInteractor<CreateShipInputPort, ICreateShipOutputPort>> m_Interactor;
+
+        #endregion Fields
+  
         #region - - - - - - Handle Tests - - - - - -
 
         [Test]
@@ -16,6 +25,7 @@ namespace Tests.Backend.UseCases.ShipUseCases.CreateShipUseCase
             // Act
             
             // Assert
+            
         }
 
         #endregion Handle Tests
