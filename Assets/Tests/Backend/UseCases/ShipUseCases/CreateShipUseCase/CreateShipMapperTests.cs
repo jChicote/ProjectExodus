1ef8e6fb3d5ff4679d359c865ea8907410e3165a
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-10-28 21:20:04 +1100

    Write unit tests for CreateShipInteractor

---
+++ Assets/Tests/Backend/UseCases/ShipUseCases/CreateShipUseCase/CreateShipMapperTests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+
+namespace Tests.Backend.UseCases.ShipUseCases.CreateShipUseCase
+{
+
+    public class CreateShipMapperTests
+    {
+
+        #region - - - - - - Mapper Configuration Tests - - - - - -
+
+        [Test]
+        public void Map_MapCreateShipMappings_ConfigurationsExist()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+            
+        }
+
+        [Test]
+        public void Map_InvokeCreateShipMappings_MappingsAreSuccessful()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+
+        }
+
+        #endregion Mapper Configuration Tests
+  
+    }
+
+}
