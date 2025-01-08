cb9f488a562648b6380f30b3253976d66560fdaa
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-01-08 22:37:18 +1100

    Add RequiredFieldAttribute

---
+++ Assets/Scripts/Editor/RequiredFieldEditor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Provides error messages if a field through the Inspector is not provided a value if marked as 'RequiredField'.
+/// </summary>
+[CustomEditor(typeof(MonoBehaviour), true)]
+public class RequiredFieldEditor : Editor
+{
+    
+    #region - - - - - - Methods - - - - - -
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        object _TargetObject = target;
+        FieldInfo[] fields = _TargetObject.GetType()
+            .GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        foreach (var field in fields)
+        {
+            RequiredFieldAttribute _RequiredAttribute = (RequiredFieldAttribute)Attribute
+                .GetCustomAttribute(field, typeof(RequiredFieldAttribute));
+            if (_RequiredAttribute != null)
+            {
+                object _Value = field.GetValue(_TargetObject);
+                if (_Value == null || (_Value is object unityObject && unityObject == null))
+                    EditorGUILayout.HelpBox($"{field.Name} is required but not assigned.", MessageType.Error);
+            }
+        }
+    }
+
+    #endregion Methods
+
+}
---
+++ Assets/Scripts/Utility/Attributes/RequiredFieldAttribute.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Responsible for marking fields or properties that are 'required' for the component to run.
+/// </summary>
+/// <remarks>This is an aid for the Editor's Inspector to highlight fields that need to be set.</remarks>
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class RequiredFieldAttribute : PropertyAttribute
+{
+}
