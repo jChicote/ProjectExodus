1778998c000ef57d8069c243c87dca40ad1abebd
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-02-06 13:05:58 +1100

    Implement check logic for screen position check

--- Assets/Scripts/GameLogic/Enemy/BehaviourTree/Conditions/CheckTargetWithinScreenCondition.cs
+++ Assets/Scripts/GameLogic/Enemy/BehaviourTree/Conditions/CheckTargetWithinScreenCondition.cs
@@ -8,13 +8,33 @@ namespace ProjectExodus
     [MBTNode(name = "Conditions/Is Target Within Screen")]
     public class CheckTargetWithinScreenCondition : Condition   
     {
+
+        #region - - - - - - Fields - - - - - -
+
         [SerializeField] 
         private TransformReference m_TargetTransform = new();
 
+        #endregion Fields
+  
+        #region - - - - - - Methods - - - - - -
+
         public override bool Check()
         {
-            throw new System.NotImplementedException();
+            Vector2 _BottomLeftWorldPosition = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+            Vector2 _TopRightWorldPosition = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+            float _LeftViewportScreenLimit = _BottomLeftWorldPosition.x;
+            float _RightViewportScreenLimit = _TopRightWorldPosition.x;
+            float _TopViewportScreenLimit = _TopRightWorldPosition.y;
+            float _BottomViewportScreenLimit = _BottomLeftWorldPosition.y;
+
+            return this.m_TargetTransform.Value.position.x > _LeftViewportScreenLimit
+                    && this.m_TargetTransform.Value.position.x < _RightViewportScreenLimit
+                    && this.m_TargetTransform.Value.position.y > _BottomViewportScreenLimit 
+                    && this.m_TargetTransform.Value.position.y < _TopViewportScreenLimit;
         }
+
+        #endregion Methods
+  
     }
 
 }
