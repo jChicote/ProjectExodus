982a6ec33c6c5d002b876601b927d0b4ed0a97a1
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-02-13 20:24:55 +1100

    Fix translation behaviour for zeto drone

--- Assets/Scripts/GameLogic/Enemy/BehaviourTree/Tasks/TranslateToRandomScreenPosition.cs
+++ Assets/Scripts/GameLogic/Enemy/BehaviourTree/Tasks/TranslateToRandomScreenPosition.cs
@@ -35,44 +35,46 @@ namespace ProjectExodus
         private void Update()
         {
             if (!this.m_CanTravel)
+            {
                 this.m_EventTimer.TickTimer();
+                return;
+            }
+            
+            if (this.m_ElapsedTime > this.m_DurationOfTravel)
+            {
+                this.ResetTranslationValues();
+                return;
+            }
+            
+            this.RunTravel();
         }
 
         #endregion Unity Methods
 
         #region - - - - - - Methods - - - - - -
 
-        public override NodeResult Execute()
+        public override NodeResult Execute() 
+            => NodeResult.success;
+
+        private void StartTravel()
         {
-            if (!this.m_CanTravel)
-                return NodeResult.success;
+            this.m_CanTravel = true;
+            Vector3 _RandomPosition = new(Random.Range(0f, 1f), Random.Range(0f, 1f), 0);
+            Vector3 _WorldPoint = Camera.main.ViewportToWorldPoint(_RandomPosition);
+            
+            this.m_NextTravelPosition = new(_WorldPoint.x, _WorldPoint.y);
+            this.m_StartingTravelPosition = this.m_SourceTransform.Value.position;
+        }
 
-            if (this.m_ElapsedTime > this.m_DurationOfTravel)
-            {
-                this.m_CanTravel = false;
-                this.ResetTranslationValues();
-                return NodeResult.success;
-            }
-
+        private void RunTravel()
+        {
             this.m_ElapsedTime += Time.deltaTime;
             Vector2 _NewPosition = Vector2.Lerp(
                 this.m_StartingTravelPosition,
                 this.m_NextTravelPosition,
-                Mathf.Clamp(0, 1, this.m_ElapsedTime / this.m_DurationOfTravel));
+                Mathf.Clamp(this.m_ElapsedTime / this.m_DurationOfTravel, 0, 1));
 
-            Debug.Log("Translation is running.");
             this.m_SourceTransform.Value.position = new(_NewPosition.x, _NewPosition.y, 0);
-            return NodeResult.success;
-        }
-
-        private void StartTravel()
-        {
-            Debug.Log("Starting Travel. Stop the timer");
-            this.m_CanTravel = true;
-            Vector3 _RandomPosition = new(Random.Range(0f, 1f), Random.Range(0f, 1f), 0);
-            Vector3 _WorldPoint = Camera.main.ViewportToWorldPoint(_RandomPosition);
-            this.m_NextTravelPosition = new(_WorldPoint.x, _WorldPoint.y);
-            this.m_StartingTravelPosition = this.m_SourceTransform.Value.position;
         }
 
         private void ResetTranslationValues()
@@ -81,6 +83,7 @@ namespace ProjectExodus
             this.m_StartingTravelPosition = Vector2.zero;
             this.m_NextTravelPosition = Vector2.zero;
             this.m_ElapsedTime = 0;
+            this.m_CanTravel = false;
         }
 
         #endregion Methods
