169210436000dad98807c96c89589bcf86d356c2
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-02-26 00:21:58 +1100

    Fix Zeto knight behaviour

--- Assets/Scripts/GameLogic/Enemy/BehaviourTree/Conditions/HasReachedScreenBorders.cs
+++ Assets/Scripts/GameLogic/Enemy/BehaviourTree/Conditions/HasReachedScreenBorders.cs
@@ -20,9 +20,13 @@ namespace ProjectExodus
 
         #region - - - - - - Methods - - - - - -
 
-        public override bool Check() 
-            => !this.IsWithinVerticalBound(this.m_SourceTransform.Value.position.y, this.m_BorderPadding) 
-                || !this.IsWithinHorizontalBound(this.m_SourceTransform.Value.position.x, this.m_BorderPadding);
+        public override bool Check()
+        {
+            var _Condition = !this.IsWithinVerticalBound(this.m_SourceTransform.Value.position.y, this.m_BorderPadding)
+                             || !this.IsWithinHorizontalBound(this.m_SourceTransform.Value.position.x, this.m_BorderPadding);
+            Debug.Log("Has reached screen border is:" + _Condition);
+            return _Condition;
+        }
 
         // TODO: This needs to be optimised and refactored
         private bool IsWithinVerticalBound(float verticalPosition, float borderPadding)
