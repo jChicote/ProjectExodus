f3970cb8cfec547ceaaed0216509e33deebe6fd2
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-03-23 19:53:30 +1100

    Add PixelifySans font and create TMP font asset

---
+++ Assets/Scripts/UserInterface/GameplayGUI/PointsHUD/ViewElements/PointsHoverMarker.cs
@@ -0,0 +1,39 @@
+﻿using ProjectExodus.GameLogic.Common.Timers;
+using ProjectExodus.GameLogic.Pause.PausableMonoBehavior;
+using TMPro;
+using UnityEngine;
+
+public class PointsHoverMarker : PausableMonoBehavior
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    public TMP_Text m_PointsText;
+    public float m_Lifetime;
+
+    private EventTimer m_Timer;
+
+    #endregion Fields
+
+    #region - - - - - - Unity Methods - - - - - -
+
+    private void Start() 
+        => this.m_Timer = new EventTimer(this.m_Lifetime, Time.deltaTime, () => Destroy(this.gameObject), canRun: true);
+
+    private void Update()
+    {
+        if (this.m_IsPaused) return;
+        
+        this.m_Timer.TickTimer();
+    }
+
+    #endregion Unity Methods
+  
+    #region - - - - - - Methods - - - - - -
+
+    public void SetPoints(int points)
+        => this.m_PointsText.SetText(points.ToString());
+    
+    #endregion Methods
+
+}
