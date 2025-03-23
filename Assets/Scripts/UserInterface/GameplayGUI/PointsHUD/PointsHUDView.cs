d60fb6edcab77792dce1e65d9020f34fc4f0e3a4
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-03-23 19:53:48 +1100

    Create new PointsHUD MVC

---
+++ Assets/Scripts/UserInterface/GameplayGUI/PointsHUD/PointsHUDView.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PointsHUDView : MonoBehaviour
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    public TMP_Text m_TotalPointsText;
+
+    #endregion Fields
+
+    #region - - - - - - Unity Methods - - - - - -
+
+    
+
+    #endregion Unity Methods
+  
+}
