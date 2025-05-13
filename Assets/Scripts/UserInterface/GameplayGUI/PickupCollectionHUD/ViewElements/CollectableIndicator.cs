213e96b2b1cf533fa86cfeea5731e2a08867717a
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-05-13 21:26:37 +1000

    Create CollectableIndicators

---
+++ Assets/Scripts/UserInterface/GameplayGUI/PickupCollectionHUD/ViewElements/CollectableIndicator.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableIndicator : MonoBehaviour
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    [SerializeField] private Image m_IndicatorImage;
+    [SerializeField] private TMP_Text m_IndicatorCount;
+    [SerializeField] private Color m_ActiveColor;
+    [SerializeField] private Color m_InactiveColor;
+
+    #endregion Fields
+
+    #region - - - - - - Methods - - - - - -
+
+    public void SetCount(int count)
+        => this.m_IndicatorCount.text = count.ToString();
+
+    public void SetImage(Sprite sprite)
+        => this.m_IndicatorImage.sprite = sprite;
+
+    #endregion Methods
+
+}
