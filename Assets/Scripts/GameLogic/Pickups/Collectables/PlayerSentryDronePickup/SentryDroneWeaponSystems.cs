d6f94b14453d4cbe15fd6c078942f7ba1af80e13
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-05-19 23:35:36 +1000

    Add Sentry weapon systems

--- Assets/Scripts/GameLogic/Pickups/Collectables/PlayerSentryDronePickup/SentryDroneWeaponSystems.cs
+++ Assets/Scripts/GameLogic/Pickups/Collectables/PlayerSentryDronePickup/SentryDroneWeaponSystems.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using MBT;
 using ProjectExodus.GameLogic.Weapons;
+using ProjectExodus.Utility.GameValidation;
 using UnityEngine;
 
 public interface IWeaponSystem
@@ -22,22 +24,36 @@ public class SentryDroneWeaponSystems : MonoBehaviour, IWeaponSystem
     #region - - - - - - Fields - - - - - -
 
     [SerializeField] private Blackboard m_Blackboard;
-    [SerializeField] private GameObject m_WeaponObjects;
+    [SerializeField] private GameObject[] m_WeaponObjects;
 
     private IWeapon[] m_Weapons;
 
     #endregion Fields
 
+    #region - - - - - - Unity Methods - - - - - -
+
+    private void Start()
+    {
+        string _SourceObjectName = this.gameObject.name;
+        GameValidator.NotNull(this.m_Blackboard, nameof(m_Blackboard), sourceObjectName: _SourceObjectName);
+
+        this.m_Weapons = this.m_WeaponObjects.Select(w => w.GetComponent<IWeapon>()).ToArray();
+    }
+
+    #endregion Unity Methods
+  
     #region - - - - - - Methods - - - - - -
 
     public void EnableWeaponFire()
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < this.m_Weapons.Length; i++)
+            this.m_Weapons[i].ToggleWeaponFire(true);
     }
 
     public void DisableWeaponFire()
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < this.m_Weapons.Length; i++)
+            this.m_Weapons[i].ToggleWeaponFire(false);
     }
 
     #endregion Methods
